0a41f2c9fb573c9547c7428ab63b0db84279f916
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-06-29 14:38:50 +0800

    update

--- traj.sh
+++ traj.sh
@@ -1,7 +1,7 @@
 
 for ((i=2;i<11;i++))
 do
-indexstr=$((0.1*$i))
+indexstr=$(echo "$i*0.1" | bc)
 cd ./0Mpa-${indexstr}V
 echo '4' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o CL0Mpa-${indexstr}V.gro -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
 cd ..
