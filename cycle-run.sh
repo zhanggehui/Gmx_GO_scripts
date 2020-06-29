54d3567bbdea5d2d48dfb3bbc84fd40f86d9356e
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-06-29 18:48:41 +0800

    update

--- cycle-run.sh
+++ cycle-run.sh
@@ -3,7 +3,7 @@
 # run in root dir
 ttotal=5000000       #fs
 tstep=5000000        #fs
-pressure=400         #Mpa
+pressure=500         #Mpa
 nvtequdir=nvtequ
 
 ############################################################
