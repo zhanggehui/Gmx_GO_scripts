bdbc7f77a2745d96515ddcd933e3f153327eb91a
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-06-30 11:15:48 +0800

    update

--- traj.sh
+++ traj.sh
@@ -1,5 +1,5 @@
 
-for ((i=1;i<5;i++))
+for ((i=1;i<10;i++))
 do
 #cd ./0Mpa-0.${i}V
 #echo '4' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o CL0Mpa-0.${i}V.gro -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
@@ -7,11 +7,11 @@ do
 #cd ./${i}00Mpa-0V
 #echo '4' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o CL${i}00Mpa-0V.gro -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
 
-#cd ./${i}00Mpa-0V
-#echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o CA${i}00Mpa-0V.gro -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+cd ./${i}00Mpa-0V
+echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o CA${i}00Mpa-0V.gro -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
 
-cd ./0Mpa-0.${i}V
-echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o CA0Mpa-0.${i}V.gro -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+#cd ./0Mpa-0.${i}V
+#echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o CA0Mpa-0.${i}V.gro -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
 
 cd ..
 done
