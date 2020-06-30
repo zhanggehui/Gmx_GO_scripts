d56e03320680fa1d9152813e1358a96b429b2677
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-01 07:49:31 +0800

    update

--- subpressure.sh
+++ subpressure.sh
@@ -4,7 +4,6 @@ do
 word="pressure=" ; new="pressure=1${i}00     #MPa"
 sed -i "/$word/c$new" ./scripts/cycle-run.sh
 source ./scripts/auto-run.sh cycle-run.sh 1${i}00Mpa-0V
-#echo $(sed -n '1p' ./${i}00Mpa-0V/waternumber)
 done
 
 
--- subvoltage.sh
+++ subvoltage.sh
@@ -5,7 +5,6 @@ word="E-y                      ="
 new="E-y                      =  1  1.${i}  0"
 sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
 source ./scripts/auto-run.sh cycle-run.sh 0Mpa-1.${i}V
-#echo $(sed -n '1p' ./${i}00Mpa-0V/waternumber)
 done
 
 
