2780819f2cb66e630fefd56b21e5a0795b5f749e
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-06-30 22:16:56 +0800

    update

--- subvoltage.sh
+++ subvoltage.sh
@@ -4,7 +4,7 @@ do
 word="E-y                      ="
 new="E-y                      =  1  1.${i}  0"
 sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
-source ./scripts/auto-run.sh cycle-run.sh 0Mpa-0.${i}V
+source ./scripts/auto-run.sh cycle-run.sh 0Mpa-1.${i}V
 #echo $(sed -n '1p' ./${i}00Mpa-0V/waternumber)
 done
 
