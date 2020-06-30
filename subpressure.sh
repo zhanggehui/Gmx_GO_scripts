224a5214b0b97a826dbf1873ef562dbf72a31f7e
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-06-30 14:21:24 +0800

    update

--- subpressure.sh
+++ subpressure.sh
@@ -1,7 +1,7 @@
 
 for ((i=1;i<10;i++))
 do
-word="pressure=" ; new="pressure=${i}00Mpa"
+word="pressure=" ; new="pressure=${i}00     #MPa"
 sed -i "/$word/c$new" ./scripts/cycle-run.sh
 source ./scripts/auto-run.sh cycle-run.sh ${i}00Mpa-0V
 #echo $(sed -n '1p' ./${i}00Mpa-0V/waternumber)
