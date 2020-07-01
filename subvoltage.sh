d02041b5abcbc417e844a27aeeade4f4a4831985
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-01 18:11:08 +0800

    update

--- subvoltage.sh
+++ subvoltage.sh
@@ -1,10 +1,10 @@
 
-for ((i=1;i<10;i++))
+for ((i=1;i<7;i++))
 do
 word="E-y                      ="
-new="E-y                      =  1  0.${i}  0"
+new="E-y                      =  1  1.${i}  0"
 sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
-source ./scripts/auto-run.sh cycle-run.sh 0Mpa-0.${i}V
+source ./scripts/auto-run.sh cycle-run.sh 0Mpa-1.${i}V
 done
 
 
