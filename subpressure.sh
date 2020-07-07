9ea13e2e23e2c2428ad605cedf95fe8fc80be8cb
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-07 10:13:25 +0800

    update

--- subpressure.sh
+++ subpressure.sh
@@ -1,10 +1,7 @@
 
-for ((i=7;i<10;i++))
+for ((i=1;i<20;i++))
 do
-word="pressure=" ; new="pressure=${i}00     #MPa"
-sed -i "/$word/c$new" ./scripts/cycle-run.sh
-source ./scripts/auto-run.sh cycle-run.sh ${i}00Mpa-0V
+    word="pressure=" ; new="pressure=${i}00     #MPa"
+    sed -i "/$word/c$new" ./scripts/cycle-run.sh
+    source ./scripts/auto-run.sh cycle-run.sh ${i}00Mpa-0V
 done
-
-
-
