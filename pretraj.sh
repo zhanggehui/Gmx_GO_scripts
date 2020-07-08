ad88bc7a664ece76eb4bc1ba12a09da9f98fa191
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-08 15:53:29 +0800

    update

---
+++ pretraj.sh
@@ -0,0 +1,10 @@
+
+ionname=LI
+
+for ((i=1;i<20;i++))
+do
+    cd ./${i}00Mpa-0V
+    echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}${i}00Mpa-0V.gro \
+    -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+    cd ..
+done
---
+++ voltraj.sh
@@ -0,0 +1,19 @@
+
+
+ionname=LI
+
+for((k=0;k<1;k++))
+do
+    for ((i=1;i<10;i++))
+    do
+        cd ./0Mpa-${k}.${i}V
+        echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-${k}.${i}V.gro \
+        -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+        cd ..
+    done
+done
+
+cd ./0Mpa-1V
+echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-1V.gro \
+-pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+cd ..
