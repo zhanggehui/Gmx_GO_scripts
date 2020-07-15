4063272c25ad68e6a8c1b27441a242c3aceb7cb3
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-15 12:10:40 +0800

    update

--- 4laGO_pre.sh
+++ 4laGO_pre.sh
@@ -1,5 +1,5 @@
 source /home/liufeng_pkuhpc/gmx-zs.sh
-ion=CS
+ion=LI
 mv GO2-${ion}.gro GO2-ion.gro
 mv GO2-${ion}.top GO2.top
 gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ./scripts/ndxcommands.sh
