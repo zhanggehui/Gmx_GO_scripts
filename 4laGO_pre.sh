134b289cd5112c72fdfdf0d76fa3a9c570150d32
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-18 17:59:32 +0800

    update

--- 4laGO_pre.sh
+++ 4laGO_pre.sh
@@ -3,6 +3,6 @@ mv GO2-${ion}.gro GO2-ion.gro
 mv GO2-${ion}.top GO2.top
 gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ./scripts/final4la_ndxcommands.sh
 
-
 #em
 source ./scripts/auto-run.sh em.sh em
+111
