5cf31f7ffc9365a9b25f8e9367982da61738b21f
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-20 21:59:41 +0800

    update

--- pressure_traj.sh
+++ pressure_traj.sh
@@ -1,5 +1,17 @@
+source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 ionname=LI
+
+if [ ! -d ${ionname}_traj ]
+then
 mkdir ../${ionname}_traj
+fi
+
+cd ./0Mpa-0V
+echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-0V \
+-pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+cp -rf  ${ionname}0Mpa-0V ../../${ionname}_traj
+cd ..
+
 for ((i=1;i<20;i++))
 do
     cd ./${i}00Mpa-0V
@@ -9,8 +21,13 @@ do
     cp -rf  $trajname ../../${ionname}_traj
     cd ..
 done
+
+source ./scripts/voltage_traj.sh
+
 romote='liufeng_zgh@10.100.1.88:/10.100.1.5/liufeng_pkuhpc/'
 mydir='home/liufeng_pkuhpc/lustre2/zgh/zgh_4laGO/final4la/'
 trajdir=$romote$mydir${ionname}_traj/
 macbookdir='/Users/zhanggehui/Desktop/final4laGO2/receive'
 scp -r $romotedir $macbookdir
+
+
--- voltage_traj.sh
+++ voltage_traj.sh
@@ -1,17 +1,33 @@
-ionname=CS
 
-for((k=0;k<2;k++))
+ionname=LI
+
+if [ ! -d ${ionname}_traj ]
+then
+mkdir ../${ionname}_traj
+fi
+
+k=0
+for ((i=1;i<10;i++))
 do
-    for ((i=1;i<10;i++))
-    do
-        cd ./0Mpa-${k}.${i}V
-        echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-${k}.${i}V.gro \
-        -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
-        cd ..
-    done
+    cd ./0Mpa-${k}.${i}V
+    echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-${k}.${i}V.gro \
+    -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+    cp -rf  $trajname ../../${ionname}_traj
+    cd ..
+done
+
+k=1
+for ((i=1;i<7;i++))
+do
+    cd ./0Mpa-${k}.${i}V
+    echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-${k}.${i}V.gro \
+    -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+    cp -rf  $trajname ../../${ionname}_traj
+    cd ..
 done
 
 cd ./0Mpa-1V
 echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-1V.gro \
 -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+cp -rf  $trajname ../../${ionname}_traj
 cd ..
