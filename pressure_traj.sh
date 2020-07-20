74c848ba10332b0b435fb8f24fca447b87d46890
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-20 22:15:03 +0800

    update

--- pressure_traj.sh
+++ pressure_traj.sh
@@ -5,11 +5,11 @@ if [ ! -d ${ionname}_traj ]
 then
 mkdir ../${ionname}_traj
 fi
-
 cd ./0Mpa-0V
 echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-0V \
 -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
 cp -rf  ${ionname}0Mpa-0V ../../${ionname}_traj
+#rm  -rf \#*
 cd ..
 
 for ((i=1;i<20;i++))
@@ -19,15 +19,10 @@ do
     echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
     -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
     cp -rf  $trajname ../../${ionname}_traj
+    #rm  -rf \#*
     cd ..
 done
 
 source ./scripts/voltage_traj.sh
-
-romote='liufeng_zgh@10.100.1.88:/10.100.1.5/liufeng_pkuhpc/'
-mydir='home/liufeng_pkuhpc/lustre2/zgh/zgh_4laGO/final4la/'
-trajdir=$romote$mydir${ionname}_traj/
-macbookdir='/Users/zhanggehui/Desktop/final4laGO2/receive'
-scp -r $romotedir $macbookdir
 
 
--- voltage_traj.sh
+++ voltage_traj.sh
@@ -10,8 +10,10 @@ k=0
 for ((i=1;i<10;i++))
 do
     cd ./0Mpa-${k}.${i}V
-    echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-${k}.${i}V.gro \
+    trajname=${ionname}0Mpa-${k}.${i}V.gro
+    echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
     -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+    rm  -rf \#*
     cp -rf  $trajname ../../${ionname}_traj
     cd ..
 done
@@ -20,8 +22,10 @@ k=1
 for ((i=1;i<7;i++))
 do
     cd ./0Mpa-${k}.${i}V
-    echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-${k}.${i}V.gro \
+    trajname=${ionname}0Mpa-${k}.${i}V.gro
+    echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
     -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+    rm  -rf \#*
     cp -rf  $trajname ../../${ionname}_traj
     cd ..
 done
@@ -29,5 +33,6 @@ done
 cd ./0Mpa-1V
 echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${ionname}0Mpa-1V.gro \
 -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
-cp -rf  $trajname ../../${ionname}_traj
+rm  -rf \#*
+cp -rf  ${ionname}0Mpa-1V.gro ../../${ionname}_traj
 cd ..
