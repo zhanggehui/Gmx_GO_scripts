b0424198d38579545b5e26010750db0fa90340a4
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-22 19:59:59 +0800

    update

---
+++ 2_final4la_ndxcommands.sh
@@ -0,0 +1,11 @@
+a 1-1600 | a 4032-5631 | a 8063-9662 | a 12094-13693
+name 11 gra
+a 1601-2080 | a 5632-6111 | a 9663-10142 | a 13694-14173
+name 12 funcgrp
+a 1
+name 13 waterlayer
+del 8
+del 8
+del 8
+q
+
--- 4laGO_pre.sh
+++ 4laGO_pre.sh
@@ -1,13 +1,15 @@
 source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 ions=("CS" "LI" "NA" "K" "CA" "MG")
-ion=${ions[3]}
+ion=${ions[4]}
+val=${ions[4]}2 # 2 for ion 4 or 5
 if [ ! -d "./$ion" ];then
     mkdir ./$ion
     cp -r oplsaaGO.ff ./$ion
-    cp ./receive/GO2-${ion}.gro ./$ion/GO2-ion.gro
-    cp ./receive/GO2-${ion}.top ./$ion/GO2.top
+    cp ./receive/GO2-${val}.gro ./$ion/GO2-ion.gro
+    cp ./receive/GO2-${val}.top ./$ion/GO2.top
     cd ./$ion
-    gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../md_scripts/final4la_ndxcommands.sh
+    #gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../md_scripts/final4la_ndxcommands.sh
+    gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../md_scripts/2_final4la_ndxcommands.sh
     git clone https://github.com/zhanggehui/Gmx_GO_scripts.git
     mv Gmx_GO_scripts scripts
     cp ../md_scripts/.git/config ./scripts/.git/config2
