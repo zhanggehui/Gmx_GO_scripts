b1f727bdbf4ba60cf5b774bcbd4a74dae3ae9c6e
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-27 17:29:26 +0800

    update

--- lay_pressure_traj.sh
+++ lay_pressure_traj.sh
@@ -1,30 +1,31 @@
 source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 
 spacing=1.1
-
+trajdir=lay_${spacing}_traj
 #rm -rf ../lay${spacing}_traj
-if [ ! -d lay${spacing}_traj ] ; then
-mkdir ../lay${spacing}_traj
+if [ ! -d $trajdir ] ; then
+mkdir ../$trajdir
 
 cd ./0Mpa-0V
-echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${spacing}0Mpa-0V.gro \
+trajname=0Mpa-0V.gro
+echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
 -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
-cp -rf  ${spacing}0Mpa-0V.gro ../../lay${spacing}_traj
+cp -rf  $trajname ../../$trajdir
 rm  -rf \#*
 cd ..
 
 for ((i=1;i<20;i++))
 do
     cd ./${i}00Mpa-0V
-    trajname=${spacing}${i}00Mpa-0V.gro
+    trajname=${i}00Mpa-0V.gro
     echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
     -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
-    cp -rf  $trajname ../../lay${spacing}_traj
+    cp -rf  $trajname ../../$trajdir
     rm  -rf \#*
     cd ..
 done
 
-source ./scripts/voltage_traj.sh
+source ./scripts/lay_voltage_traj.sh
 
 else
 echo 'already exists!'
--- lay_voltage_traj.sh
+++ lay_voltage_traj.sh
@@ -2,11 +2,11 @@ k=0
 for ((i=1;i<10;i++))
 do
     cd ./0Mpa-${k}.${i}V
-    trajname=${spacing}0Mpa-${k}.${i}V.gro
+    trajname=0Mpa-${k}.${i}V.gro
     echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
     -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+    cp -rf  $trajname ../../$trajdir
     rm  -rf \#*
-    cp -rf  $trajname ../../lay${spacing}_traj
     cd ..
 done
 
@@ -14,17 +14,18 @@ k=1
 for ((i=1;i<7;i++))
 do
     cd ./0Mpa-${k}.${i}V
-    trajname=${spacing}0Mpa-${k}.${i}V.gro
+    trajname=0Mpa-${k}.${i}V.gro
     echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
     -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+    cp -rf  $trajname ../../$trajdir
     rm  -rf \#*
-    cp -rf  $trajname ../../lay${spacing}_traj
     cd ..
 done
 
 cd ./0Mpa-1V
-echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o ${spacing}0Mpa-1V.gro \
+trajname=0Mpa-1V.gro
+echo '3' | gmx trjconv -f nvt-pro-traj.trr -s traj.tpr -o $trajname \
 -pbc nojump -b 0 -e 5000 -skip 5000 -n waterlayer.ndx
+cp -rf $trajname ../../$trajdir
 rm  -rf \#*
-cp -rf  ${spacing}0Mpa-1V.gro ../../lay${spacing}_traj
 cd ..
