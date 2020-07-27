aecf77da7c03fb773054005cd96f3ae2261954e3
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-27 09:30:10 +0800

    update

---
+++ lay_spacing_pre.sh
@@ -0,0 +1,21 @@
+source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
+spacing=1.1
+dir=lay_$spacing
+if [ ! -d "./$dir" ];then
+    mkdir ./$dir
+    cp -r oplsaaGO.ff ./$dir
+    cp ./receive/GO_${spacing}.gro ./$dir/GO2-ion.gro
+    cp ./receive/GO_${spacing}.top ./$dir/GO2.top
+    cd ./$dir
+    #gmx make_ndx -f GO2-dir.gro -o waterlayer.ndx  < ../md_scripts/final4la_ndxcommands.sh
+    gmx make_ndx -f GO2-dir.gro -o waterlayer.ndx  < ../md_scripts/ndx_${spacing}.sh
+    git clone https://github.com/zhanggehui/Gmx_GO_scripts.git
+    mv Gmx_GO_scripts scripts
+    cp ../md_scripts/.git/config ./scripts/.git/config2
+    mv -f ./scripts/.git/config2 ./scripts/.git/config
+    source ./scripts/auto-run.sh em.sh em
+    cd ..
+else
+   echo 'already exist!'
+fi
+
---
+++ ndx_1.1.sh
@@ -0,0 +1,10 @@
+a 1-1600 |a 4459-6058 |a 8917-10516 |a 13375-14974
+name 11 gra
+a 1601-2080 |a 6059-6538 |a 10517-10996 |a 14975-15454
+name 12 funcgrp
+a 1
+name 13 waterlayer
+del 8
+del 8
+del 8
+q
