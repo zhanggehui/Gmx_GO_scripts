dc210256ba8c125450062778548ac4247be0aed4
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-28 17:21:35 +0800

    update

--- mix_subvoltage.sh
+++ mix_subvoltage.sh
@@ -1,5 +1,7 @@
-#shut off pressure
-word="pressure=" ; new="pressure=1500     #Mpa"
+pressure=1500
+
+#choose pressure
+word="pressure=" ; new="pressure=${pressure}     #Mpa"
 sed -i "/$word/c$new" ./scripts/cycle-run.sh
 
 #0.x V
@@ -8,14 +10,14 @@ do
 word="electric-field-y"
 new="electric-field-y         = -0.${i} 0 0 0"
 sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
-source ./scripts/auto-run.sh cycle-run.sh 0Mpa-0.${i}V
+source ./scripts/auto-run.sh cycle-run.sh ${pressure}Mpa-0.${i}V
 done
 
 #1V
 word="electric-field-y"
 new="electric-field-y         = -1 0 0 0"
 sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
-source ./scripts/auto-run.sh cycle-run.sh 0Mpa-1V
+source ./scripts/auto-run.sh cycle-run.sh ${pressure}Mpa-1V
 
 #1.x V
 for ((i=1;i<7;i++))
@@ -23,7 +25,7 @@ do
 word="electric-field-y"
 new="electric-field-y         = -1.${i} 0 0 0"
 sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
-source ./scripts/auto-run.sh cycle-run.sh 0Mpa-1.${i}V
+source ./scripts/auto-run.sh cycle-run.sh ${pressure}Mpa-1.${i}V
 done
 
 
