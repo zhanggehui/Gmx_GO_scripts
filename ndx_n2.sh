347b4c88935f3da4eec93b03c2b0529c04b8b9f3
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-29 18:15:14 +0800

    update

---
+++ ndx_n2.sh
@@ -0,0 +1,11 @@
+a 1-1600 |a 4073-5672 |a 8145-9744 |a 12217-13816
+name 13 gra
+a 1601-2080 |a 5673-6152 |a 9745-10224 |a 13817-14296
+name 14 funcgrp
+a 1
+name 15 waterlayer
+del 9
+del 9
+del 9
+del 9
+q
---
+++ negative_pre.sh
@@ -0,0 +1,21 @@
+source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
+
+negative=n2
+dir=negative_$negative
+if [ ! -d "./$dir" ];then
+    mkdir ./$dir
+    cp -r oplsaaGO.ff ./$dir
+    cp ./receive/GO_${negative}.gro ./$dir/GO2-ion.gro
+    cp ./receive/GO_${negative}.top ./$dir/GO2.top
+    cd ./$dir
+    gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../md_scripts/ndx_${negative}.sh
+    git clone https://github.com/zhanggehui/Gmx_GO_scripts.git
+    mv Gmx_GO_scripts scripts
+    cp ../md_scripts/.git/config ./scripts/.git/config2
+    mv -f ./scripts/.git/config2 ./scripts/.git/config
+    source ./scripts/auto-run.sh em.sh em
+    cd ..
+else
+   echo 'already exist!'
+fi
+
