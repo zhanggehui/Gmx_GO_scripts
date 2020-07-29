cf05c3b0ea7c2bab28b1b8327fc6e1f522158242
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-29 12:28:44 +0800

    update

---
+++ ndx_32.sh
@@ -0,0 +1,10 @@
+a 1-1600 |a 4069-5668 |a 8137-9736 |a 12205-13804
+name 11 gra
+a 1601-2080 |a 5669-6148 |a 9737-10216 |a 13805-14284
+name 12 funcgrp
+a 1
+name 13 waterlayer
+del 8
+del 8
+del 8
+q
