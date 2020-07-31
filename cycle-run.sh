e0fdacd6b712615e920a8aa89b841a5f3d9ced11
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-07-31 17:23:10 +0800

    update

--- cycle-run.sh
+++ cycle-run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+# environment variable: orientation ; rundir ; runscript ; scriptsdir
 # run in root dir
-ttotal=5000000       #fs
-tstep=5000000        #fs
-pressure=1900        #Mpa
+ttotal=5000000       #fs shell limit in integer
+tstep=5000000        #fs >1000 at least
+export pressure=1900        #Mpa
 nvtequdir=nvtequ
 ############################################################
 #first initialization
 #source command executed in current shell so pay attentation to environment variable
-echo "pressure: $pressure" > ./$rundir/waternumber
-echo '' >> ./$rundir/waternumber
+echo "pressure: $pressure" > ./$rundir/waternumber ; echo '' >> ./$rundir/waternumber
 echo 'stepnum   count   acceleration' >> ./$rundir/waternumber
 numofcycle=$(($ttotal/$tstep))
 
@@ -28,42 +28,35 @@ fi
 if [ -f ./$nvtequdir/nvt-equ.cpt ] ; then
 mv ./$nvtequdir/nvt-equ.cpt ./$nvtequdir/nvt-step-0.cpt
 fi
-mdpfile=./$rundir/nvt-cycle.mdp
-
+mdpfile=./$rundir/nvt-cycle.mdp ; topfile=GO2.top
+ndxfile=./$rundir/waterlayer.ndx
 #posregro=./$nvtequdir/nvt-step-0.gro
 
-for((i=1;i<=$numofcycle;i++))
-do
-tprname=nvt-step-$i.tpr
-if [ $i -eq 1 ]
-then
-lastgro=./$nvtequdir/nvt-step-0.gro
-lastcpt=./$nvtequdir/nvt-step-0.cpt
+for((i=1;i<=$numofcycle;i++)) ; do
+tprname=nvt-step-$i.tpr ; export i
+if [ $i -eq 1 ] ; then
+lastgro=./$nvtequdir/nvt-step-$((i-1)).gro ; lastcpt=./$nvtequdir/nvt-step-$((i-1)).cpt
 else
-lastgro=./$rundir/nvt-step-$((i-1)).gro
-lastcpt=./$rundir/nvt-step-$((i-1)).cpt
+lastgro=./$rundir/nvt-step-$((i-1)).gro ; lastcpt=./$rundir/nvt-step-$((i-1)).cpt
 fi
 
-tinit=$((i-1))
-resettinit="tinit                    = $tinit"
+tinit=$(( (i-1)*(tstep/1000) )) ; resettinit="tinit                    = $tinit"
 sed -i "/tinit/c$resettinit" $mdpfile
 
-./$scriptsdir/findwatersinlayer.sh $lastgro ./$rundir/waterlayer.ndx $i $ndxdir $orientation $pressure $mdpfile
+source ./$scriptsdir/findwatersinlayer.sh
 
-gmx grompp -f $mdpfile -c $lastgro -t $lastcpt \
--p GO2.top -o ./$rundir/$tprname -po $mdpdir/step$i -n ./$rundir/waterlayer.ndx
+gmx grompp -f $mdpfile -c $lastgro -t $lastcpt -p $topfile -o ./$rundir/$tprname \
+-po $mdpdir/step$i -n $ndxfile
 # -r $posregro   -maxwarn 1
 
-cd $rundir
-$gmxrun -v -deffnm ${tprname%.*}
-cd ..
+cd $rundir ; $gmxrun -v -deffnm ${tprname%.*} ; cd ..
 echo $i >> ${rundir}recordcycle
 done
 
+if [ $i -gt 1 ] ; then
 cd $rundir
 gmx trjcat -f *.trr -o nvt-pro-traj.trr
 cd ..
-
 #####after run#############################
 rm -rf ./$rundir/tmp
 mv ${rundir}recordcycle ./$rundir/recordcycle
@@ -75,3 +68,4 @@ rm -rf ./$rundir/*.cpt
 rm -rf ./$rundir/nvt-step-*.trr
 rm -rf ./$rundir/nvt-step-*.gro
 rm -rf ./$rundir/nvt-step-*.tpr
+fi
--- findwatersinlayer.sh
+++ findwatersinlayer.sh
@@ -1,93 +1,56 @@
 #!/bin/bash
 
-grofile=$1
-ndxfile=$2
-export stepnum=$3
-ndxdir=$4
-export ori=$5
-export pre=$6
-mdpfile=$7
-
-oldndxfile="./$rundir/$stepnum.ndx"
-
-mv $ndxfile $oldndxfile
-
-export boxlengthline=$(sed  -n '$p'  $grofile)
+oldndxfile="./$rundir/$i.ndx" ; mv $ndxfile $oldndxfile
+export boxlengthline=$(sed -n '$p' $lastgro)
 
 awk '
     BEGIN{pr=1;}
-  
     {
-    if(pr==1)
-    {print $0  ;}
-    if(match($0, /waterlayer/))
-    {pr=0;}
+        if(pr==1) {print $0;}
+        if( match($0,/waterlayer/) ){pr=0;}
     }
-    
-   ' $oldndxfile > $ndxfile
+   '$oldndxfile > $ndxfile
 
 mv $oldndxfile $ndxdir
 
-    awk '
-        BEGIN{
-        dir=ENVIRON["rundir"];
-        orientation=ENVIRON["ori"];
-        orientation=orientation+0;
-        if(orientation==1)
-        {p1=21;pbox1=1;pbox2=11;pbox3=21;}
-        else if(orientation==2)
-        {p1=29;pbox1=11;pbox2=1;pbox3=21;}
-        else
-        {p1=37;pbox1=21;pbox2=1;pbox3=11;}
-        len=substr(ENVIRON["boxlengthline"],pbox1,10);
-        len=len+0;
-        araalen1=substr(ENVIRON["boxlengthline"],pbox2,10);
-        araalen1=araalen1+0;
-        araalen2=substr(ENVIRON["boxlengthline"],pbox3,10);
-        araalen2=araalen2+0;
-        area=araalen1*araalen2;
-        coord=0; count=0;
-        vcoord=0; vcount=0;
-        acceleration=0;
-        presseure=ENVIRON["pre"];
-        presseure=presseure+0;
-        thick=100;   
-       }
-        
-        {
-          coord=substr($0,p1,8); coord=coord+0;
-          if( match($0, /OW/) && ( coord<thick || coord>(len-thick) ) )
-          {
-            count++;
-            serial=substr($0,16,5);
-            vcoord=substr($0,37,44); vcoord=vcoord+0;
-            if ( vcoord>=-100 && vcoord<=100)
-            {
-              vcount=vcount+1;
-              if(vcount%15!=0)
-                {printf("%5s ", serial) ;}
-              else
-                {printf("%5s\n", serial) ;}
+awk '
+    BEGIN{
+          dir=ENVIRON["rundir"];
+          orientation=ENVIRON["orientation"]; orientation=orientation+0;
+          if(orientation==1) {p1=21;pbox1=1;pbox2=11;pbox3=21;}
+          else if(orientation==2) {p1=29;pbox1=11;pbox2=1;pbox3=21;}
+          else {p1=37;pbox1=21;pbox2=1;pbox3=11;}
+          len=substr(ENVIRON["boxlengthline"],pbox1,10); len=len+0;
+          araalen1=substr(ENVIRON["boxlengthline"],pbox2,10); araalen1=araalen1+0;
+          araalen2=substr(ENVIRON["boxlengthline"],pbox3,10); araalen2=araalen2+0;
+          area=araalen1*araalen2;
+          coord=0; count=0; vcoord=0; vcount=0; pv=37; acceleration=0;
+          presseure=ENVIRON["presseure"]; presseure=presseure+0;
+          thick=100;
+         }
+    /OW/{
+         coord=substr($0,p1,8); coord=coord+0;
+         if( coord<thick || coord>(len-thick) )
+            { count++; serial=substr($0,16,5);
+              vcoord=substr($0,pv,8); vcoord=vcoord+0;
+              if ( vcoord>=-100 && vcoord<=100)
+              { vcount=vcount+1;
+                if(vcount%15!=0) {printf("%5s ", serial) ;}
+                else {printf("%5s\n", serial) ;}
+              }
             }
-          }
+         }
+     END{ acceleration=0.602*presseure*area/(vcount*18);
+          print acceleration > dir"""/tmp" ;
+          print (ENVIRON["i"], count, acceleration) >> dir"""/waternumber" ;
         }
-        
-        END{
-         acceleration=0.602*presseure*area/(count*18);
-         print acceleration > dir"""/tmp" ;
-         print (ENVIRON["stepnum"], count, acceleration) >> dir"""/waternumber" ;
-          }
-          ' $grofile >> $ndxfile
-
+    '$lastgro >> $ndxfile
 echo '' >> $ndxfile
-
 acceleration=$(sed -n '1p' ./$rundir/tmp)
 str1=accelerate
-if [ $ori -eq 1 ]
-then
+if [ $orientation -eq 1 ] ; then
 str2="accelerate               = $acceleration 0 0"
-elif [ $ori -eq 2 ]
-then
+elif [ $ori -eq 2 ] ; then
 str2="accelerate               = 0 $acceleration 0"
 else
 str2="accelerate               = 0 0 $acceleration"
