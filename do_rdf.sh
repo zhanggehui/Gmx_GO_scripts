8df8ff5387c40f182ca2d2a70bfbe42d478e3be8
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-05 21:53:57 +0800

    update

--- do_rdf.sh
+++ do_rdf.sh
@@ -2,7 +2,7 @@ source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 ions=("CS" "LI" "NA" "K" "CA" "MG")
 num=${#ions[@]}
 rdfdir=rdfs
-if [ ! -d $rdfdir ]
+if [ ! -d $rdfdir ] ; then
 mkdir $rdfdir
 
 for((i=0;i<$num;i++)) ; do
