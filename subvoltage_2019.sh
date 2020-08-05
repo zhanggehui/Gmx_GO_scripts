b06aab3c89a683b7092543cea06300bc773a3626
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-05 18:05:49 +0800

    update

--- subvoltage_2019.sh
+++ subvoltage_2019.sh
@@ -3,19 +3,19 @@ word="pressure=" ; new="pressure=0     #Mpa"
 sed -i "/$word/c$new" ./scripts/cycle-run.sh
 
 #0.x V
-#for ((i=1;i<10;i++))
-#do
-#word="electric-field-y"
-#new="electric-field-y         = 0.${i} 0 0 0"
-#sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
-#source ./scripts/auto-run.sh cycle-run.sh 0Mpa-0.${i}V
-#done
+for ((i=1;i<10;i++))
+do
+word="electric-field-y"
+new="electric-field-y         = 0.${i} 0 0 0"
+sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
+source ./scripts/auto-run.sh cycle-run.sh 0Mpa-0.${i}V
+done
 
 #1V
-#word="electric-field-y"
-#new="electric-field-y         = 1 0 0 0"
-#sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
-#source ./scripts/auto-run.sh cycle-run.sh 0Mpa-1V
+word="electric-field-y"
+new="electric-field-y         = 1 0 0 0"
+sed -i "/$word/c$new" ./scripts/nvt-cycle.mdp
+source ./scripts/auto-run.sh cycle-run.sh 0Mpa-1V
 
 #1.x V
 for ((i=1;i<7;i++))
