f094af8a03d7a246b2704414d88c796b6835183b
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-05 18:01:54 +0800

    update

---
+++ ndxs/ndx_2.sh
@@ -0,0 +1,10 @@
+a 1-1600 |a 8248-9847 |a 16495-18094 |a 24742-26341
+name 11 gra
+a 1601-2080 |a 9848-10327 |a 18095-18574 |a 26342-26821
+name 12 funcgrp
+a 1
+name 13 waterlayer
+del 8
+del 8
+del 8
+q
