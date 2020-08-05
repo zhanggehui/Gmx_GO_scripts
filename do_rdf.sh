5e7cb2df76b95a7151e559e8a458b89faffaf51c
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-05 21:58:35 +0800

    update

--- do_rdf.sh
+++ do_rdf.sh
@@ -11,7 +11,8 @@ cd ./$ion/nvtequ
 grofile=nvt-step-0.gro ; xvgfile=${ion}_rdf.xvg
 gmx make_ndx -f $grofile < ../../md_scripts/rdf_ndx.sh
 gmx rdf -f $grofile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile
-mv $xvgfile ../../$rdfdir
+cp $xvgfile ../../$rdfdir
+rm -rf \#
 cd ../../
 done
 
