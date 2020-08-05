e7a5b90e763813b512038281637e6dee95064309
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-05 21:52:26 +0800

    update

---
+++ do_rdf.sh
@@ -0,0 +1,20 @@
+source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
+ions=("CS" "LI" "NA" "K" "CA" "MG")
+num=${#ions[@]}
+rdfdir=rdfs
+if [ ! -d $rdfdir ]
+mkdir $rdfdir
+
+for((i=0;i<$num;i++)) ; do
+ion=${ions[$i]}
+cd ./$ion/nvtequ
+grofile=nvt-step-0.gro ; xvgfile=$ion_rdf.xvg
+gmx make_ndx -f $grofile < ../../md_scripts/rdf_ndx.sh
+gmx rdf -f $grofile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile
+mv $xvgfile ../../$rdfdir
+cd ../../
+done
+
+else
+echo "already exits!"
+fi
