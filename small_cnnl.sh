2e2fa45813d7525fdc2e3f3c78374a5e269d34c2
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-07 15:30:06 +0800

    update

--- small_cnnl.sh
+++ small_cnnl.sh
@@ -2,15 +2,15 @@
 #SBATCH -J gmx_analyse
 #SBATCH -p cn_nl
 #SBATCH -N 1
-#SBATCH -o ./1.out
-#SBATCH -e ./2.err
+#SBATCH -o ./3.out
+#SBATCH -e ./4.err
 #SBATCH --no-requeue
 #SBATCH -A liufeng_g1
 #SBATCH --qos=liufengcnnl
 
 hosts=`scontrol show hostname $SLURM_JOB_NODELIST` ; echo $hosts
 
-analyse_scripts=rdf_pv.sh
+analyse_scripts=do_rdf.sh
 
 source ./md_scripts/${analyse_scripts}
 
