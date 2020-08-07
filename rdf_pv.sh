ca094d68c91c311b73d3b5008e4c1d36b16ebe7f
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-07 12:27:50 +0800

    update

--- rdf_pv.sh
+++ rdf_pv.sh
@@ -43,6 +43,15 @@ cp $xvgfile ../../$rdfdir/$ion
 rm -rf \#*
 cd ../
 
+cd 0Mpa-1V ; grofile=last.gro
+xvgfile=${ion}_1V.xvg
+gmx make_ndx -f $grofile < ../../md_scripts/rdf_ndx.sh
+gmx rdf -f $grofile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile
+#-bin 0.01
+cp $xvgfile ../../$rdfdir/$ion
+rm -rf \#*
+cd ../
+
 j=0
 for ((k=1;k<10;k++)) ; do
 cd 0Mpa-$j.${k}V ; grofile=last.gro
