aba709c65d9367b8dc2e61f3962ef2c633c9cf41
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-13 21:07:12 +0800

    update

--- do_density.sh
+++ do_density.sh
@@ -1,13 +1,13 @@
 source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 ions=("CS" "LI" "NA" "K" "CA" "MG")
 num=${#ions[@]}
-densitydir=density_1500Mpa_0V
+densitydir=density_1900Mpa_0V
 if [ ! -d $densitydir ] ; then
 mkdir $densitydir
 
 for((i=0;i<$num;i++)) ; do
 ion=${ions[$i]}
-cd ./$ion/1500Mpa-0V ; ffile=nvt-pro-traj.trr
+cd ./$ion/1900Mpa-0V ; ffile=nvt-pro-traj.trr
 xvgfile=${ion}_density.xvg
 echo q | gmx make_ndx -f last.gro
 echo 3 | gmx density -f $ffile -n index.ndx -s traj.tpr -dens number -d Y -o $xvgfile -b 0 -e 5000
