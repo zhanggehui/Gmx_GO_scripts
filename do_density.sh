a00a7859ca159b0f1131ef3f02ce3597b5af3a00
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-13 21:31:37 +0800

    update

--- do_density.sh
+++ do_density.sh
@@ -10,7 +10,8 @@ ion=${ions[$i]}
 cd ./$ion/1900Mpa-0V ; ffile=nvt-pro-traj.trr
 xvgfile=${ion}_density.xvg
 echo q | gmx make_ndx -f last.gro
-echo 3 | gmx density -f $ffile -n index.ndx -s traj.tpr -dens number -d Y -o $xvgfile -b 0 -e 5000 -sl 20
+echo 5 | gmx density -f $ffile -n index.ndx -s traj.tpr -dens number -d Z -o $xvgfile -b 0 -e 5000 -sl 50
+#3 ion; 5 water
 cp $xvgfile ../../$densitydir
 rm -rf \#*
 cd ../../
