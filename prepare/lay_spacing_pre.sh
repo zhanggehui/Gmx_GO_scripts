59826c31f92db9662ab07e5bedae0d001c4294e1
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-21 17:28:19 +0800

    update

--- prepare/lay_spacing_pre.sh
+++ prepare/lay_spacing_pre.sh
@@ -1,17 +1,17 @@
 source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 
-spacing=2.5
-dir=lay_$spacing
+spacing=2 ; ion=CS
+dir=${ion}_lay/lay_$spacing
 if [ ! -d "./$dir" ];then
     mkdir ./$dir
     cp -r oplsaaGO.ff ./$dir
-    cp ./receive/GO_${spacing}.gro ./$dir/GO2-ion.gro
-    cp ./receive/GO_${spacing}.top ./$dir/GO2.top
+    cp ./receive/${ion}_lay/GO_${spacing}.gro ./$dir/GO2-ion.gro
+    cp ./receive/${ion}_lay/GO_${spacing}.top ./$dir/GO2.top
     cd ./$dir
-    gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../md_scripts/ndxs/ndx_${spacing}.sh
+    gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../md_scripts/ndxs/${ion}_lay/ndx_${spacing}.sh
     git clone https://github.com/zhanggehui/Gmx_GO_scripts.git
     mv Gmx_GO_scripts scripts
-    cp ../md_scripts/.git/config ./scripts/.git/config2
+    cp ../../md_scripts/.git/config ./scripts/.git/config2
     mv -f ./scripts/.git/config2 ./scripts/.git/config
     source ./scripts/auto-run.sh em.sh em
     cd ..
