f94cf2f743475d37a0250d467e3921665ce6b689
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-21 17:30:55 +0800

    update

--- prepare/lay_spacing_pre.sh
+++ prepare/lay_spacing_pre.sh
@@ -8,13 +8,13 @@ if [ ! -d "./$dir" ];then
     cp ./receive/${ion}_lay/GO_${spacing}.gro ./$dir/GO2-ion.gro
     cp ./receive/${ion}_lay/GO_${spacing}.top ./$dir/GO2.top
     cd ./$dir
-    gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../md_scripts/ndxs/${ion}_lay/ndx_${spacing}.sh
+    gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx  < ../../md_scripts/ndxs/${ion}_lay/ndx_${spacing}.sh
     git clone https://github.com/zhanggehui/Gmx_GO_scripts.git
     mv Gmx_GO_scripts scripts
     cp ../../md_scripts/.git/config ./scripts/.git/config2
     mv -f ./scripts/.git/config2 ./scripts/.git/config
     source ./scripts/auto-run.sh em.sh em
-    cd ..
+    cd ../../
 else
    echo 'already exist!'
 fi
